f2223cd903725fc3ed15dadacd68546e3e8d8bc5
Pierre <pierre.jacquot@cassin-macon.fr>
2016-02-28 11:25:10 +0100

    Création du constructeur de la classe Fenetre.

--- vue/fenetre.py
+++ vue/fenetre.py
@@ -1,2 +1,10 @@
 ﻿# Créé par Pierre, le 28/02/2016 en Python 3.2
 
+import pygame
+
+def __init__(self, titre, largeur, hauteur):
+    pygame.init()
+    self.fenetre = pygame.display.set_mode((largeur, hauteur))
+    fond = pygame.image.load("hakase_nyan.png").convert()
+    self.fenetre.blit(fond, (0, 0))
+
