22774f91aa4ec466e4489b32c346bf8e62fe6075
Pierre <pierre.jacquot@cassin-macon.fr>
2016-02-28 11:56:33 +0100

    Ajout d'un titre pour la fenêtre, de commentaires et d'une fonction fermer.

--- vue/fenetre.py
+++ vue/fenetre.py
@@ -3,14 +3,18 @@
 import pygame
 
 class Fenetre:
+    """Classe Fenêtre, s'occupant de l'affichage."""
+
     def __init__(self, titre, largeur, hauteur):
         pygame.init()
         self.fenetre = pygame.display.set_mode((largeur, hauteur))
-        fond = pygame.image.load("hakase_nyan.png").convert()
+        pygame.display.set_caption(titre)
+        fond = pygame.image.load("hakase_nyan.png").convert_alpha()
         self.fenetre.blit(fond, (0, 0))
         #TODO : ajouter un titre à la fenêtre.
 
     def __del__(self):
+        """Possible qu'on n'ait pas à se servir de cette fonction, mais je l'ai créée quand même au cas où."""
         pygame.quit()
 
     def rafraichir(self):
