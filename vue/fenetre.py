49f4491ad66f9fe6a65275f4b916cc7827f7160b
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-02 17:37:41 +0100

    Ajout d'une liste pour gérer l'affichage, et de fonctions qui vont avec.

--- vue/fenetre.py
+++ vue/fenetre.py
@@ -9,15 +9,24 @@ class Fenetre:
     def __init__(self, titre, largeur, hauteur):
         pygame.init()
         self.fenetre = pygame.display.set_mode((largeur, hauteur))
+        self.imgList = []
         pygame.display.set_caption(titre)
-        fond = pygame.image.load("hakase_nyan.png").convert_alpha()
-        self.fenetre.blit(fond, (0, 0))
+        #self.imgList.append(image.load("hakase_nyan.png").convert_alpha())
+        #self.fenetre.blit(fond, (0, 0))
 
     def __del__(self):
         """Possible qu'on n'ait pas à se servir de cette fonction, mais je l'ai créée quand même au cas où."""
         pygame.quit()
 
+
+    def addImgList(self, img, pos):
+        lis = [img, pos]
+        self.imgList.append(lis)
+
     def rafraichir(self):
+        #print(self.imgList)
+        for lis in self.imgList:
+            self.fenetre.blit(lis[0], lis[1])
         pygame.display.flip()
 
     def fermer(self):
@@ -26,6 +35,7 @@ class Fenetre:
 
 if __name__ == "__main__":
     f = Fenetre("test", 1000, 800)
-    f.rafraichir()
-    time.sleep(5)
+    f.addImgList(pygame.image.load("hakase_nyan.png").convert_alpha(), (0, 0))
+    f.rafraichir();
+    time.sleep(2)
     f.fermer()
