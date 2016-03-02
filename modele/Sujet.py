9fcd8948b633fba3170a9be84d789fe855028e78
DantCold <dantcollide@gmail.com>
2016-03-02 20:53:02 +0100

    classe du personnage normalement

---
+++ modele/Sujet.py
@@ -0,0 +1,9 @@
+﻿# Créé par Baptiste Noblet, le 02/03/2016 en Python 3.2
+
+class Sujet:
+    def __init__(self,x=120,y=10):
+        self.x=x  #à changer quand on aura la fenetre
+        self.y=y
+
+
+
