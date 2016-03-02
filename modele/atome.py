fd5b4c8a14b74c2fad7d80bf45736a9e3c56579e
DantCold <dantcollide@gmail.com>
2016-03-02 17:09:56 +0100

    tests sur l'heredite

--- modele/atome.py
+++ modele/atome.py
@@ -1,2 +1,19 @@
 ﻿# Créé par Baptiste Noblet, le 02/03/2016 en Python 3.2
 
+class Atome:
+
+    def __init__(self,hp,x,y) :
+
+        self.hp=hp
+        self.x=x
+        self.y=y
+
+    def Boom(self) :
+        print("Boom la molécule explose !")
+
+
+class Hydrogene(Atome):
+    def __init__(self,hp,x,y):
+        self.hp=hp
+        self.x=x
+        self.y=y
