b1122ba623f29566112719a3c0d5e353be3e79b7
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-03 18:31:57 +0100

    Surcharge du constructeur du Projectile, pour qu'il puisse viser une cible.

--- vue/projectiles.py
+++ vue/projectiles.py
@@ -4,8 +4,18 @@ class Projectile:
     """Classe des projectiles tirés par les atomes."""
 
     def __init__(self, pos, mv):
+        """Constructeur 'tout simple'..."""
         self.position = pos #tuple de position.
         self.mouvement = mv #tuple de mouvement.
+
+    def __init__(self, pos, posCible):
+        """Le fameux constructeur qui va permettre de viser le joueur."""
+        self.position = pos
+        x1, y1 = pos
+        x2, y2 = posCible
+        a = int((y2-y1)/(x2-x1))
+        b = y1 - a*x1
+        self.mouvement = (a,b)
 
     def move(self):
         self.position += self.mouvement
