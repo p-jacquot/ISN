075e1d43f1f77835e3d6cec2ee9850f9b50abdc4
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-03 19:05:51 +0100

    Ajout d'une variable modele qui fera le lien avec le modele.

--- modele/atome.py
+++ modele/atome.py
@@ -1,12 +1,14 @@
 ﻿# Créé par Baptiste Noblet, le 02/03/2016 en Python 3.2
 
 from math import *
+from projectiles import Projectile
 
 class Atome:
 
-    def __init__(self, hp, pos) :
+    def __init__(self, hp, pos, modele) :
         self.hp=hp
-        self.position = pos
+        self.position = pos #pos : tuple de la forme (positionX, positionY)
+        self.modele = modele
 
     def Boom(self) :
         print("Boom la molécule explose !")
@@ -18,11 +20,12 @@ class Atome:
 
 class Hydrogene(Atome):
 
-    def __init__(self , hp, pos):
-        Atome.__init__(hp, pos)
+    def __init__(self , hp, pos, modele):
+        Atome.__init__(hp, pos, modele)
         #Attention les enfants, ce constructeur ne sert à rien pour l'instant !
 
 
-    def tir(self,xCible,yCible):
-        distanceCible = int(sqrt(pow(xCible-self.x,2)+pow(yCible-self.y,2)))
-        print(distanceCible)
+    def tir(self, posCible):
+        #distanceCible = int(sqrt(pow(xCible-self.x,2)+pow(yCible-self.y,2)))
+        #print(distanceCible)
+        self.modele.add(Projectile(self.position, posCible)) # ce ne sera peut être pas tout à fait la méthode add(), du modèle,
