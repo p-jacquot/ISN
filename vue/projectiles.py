682e15044691691a03b8e32f1d0f8f507ebbf60e
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-03 18:16:24 +0100

    Création de la classe Projectile.

---
+++ vue/projectiles.py
@@ -0,0 +1,12 @@
+﻿# Créé par Pierre, le 03/03/2016 en Python 3.2
+
+class Projectile:
+    """Classe des projectiles tirés par les atomes."""
+
+    def __init__(self, pos, mv):
+        self.position = pos #tuple de position.
+        self.mouvement = mv #tuple de mouvement.
+
+    def move(self):
+        self.position += self.mouvement
+
