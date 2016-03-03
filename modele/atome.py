9e355a8564f53692ad7360b470d9fede496ba6b8
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-03 18:08:37 +0100

    Ajout d'une fonction tir() dans la classe mère Atome.

--- modele/atome.py
+++ modele/atome.py
@@ -1,16 +1,20 @@
 ﻿# Créé par Baptiste Noblet, le 02/03/2016 en Python 3.2
 
 from math import *
+
 class Atome:
 
     def __init__(self,hp,x,y) :
-
         self.hp=hp
         self.x=x
         self.y=y
 
     def Boom(self) :
         print("Boom la molécule explose !")
+
+    def tir(self):
+        print("Pew !")
+        #Redéfinissez cette fonction dans les classes filles.
 
 
 class Hydrogene(Atome):
@@ -21,5 +25,5 @@ class Hydrogene(Atome):
 
 
     def tir(self,xCible,yCible):
-        distanceCible=int(sqrt(pow(xCible-self.x,2)+pow(yCible-self.y,2)))
+        distanceCible = int(sqrt(pow(xCible-self.x,2)+pow(yCible-self.y,2)))
         print(distanceCible)
