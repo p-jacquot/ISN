6fc85d20e0e369da227942c5dc11ebf0f1e9d640
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-05 16:38:12 +0100

    Tracking du fichier Projectiles.py.

---
+++ vue/projectiles.py
@@ -0,0 +1,22 @@
+﻿# Créé par Pierre, le 03/03/2016 en Python 3.2
+
+class Projectile:
+    """Classe des projectiles tirés par les atomes."""
+
+    def __init__(self, pos, mv):
+        """Constructeur 'tout simple'..."""
+        self.position = pos #tuple de position.
+        self.mouvement = mv #tuple de mouvement.
+
+    def __init__(self, pos, posCible):
+        """Le fameux constructeur qui va permettre de viser le joueur."""
+        self.position = pos
+        x1, y1 = pos
+        x2, y2 = posCible
+        a = int((y2-y1)/(x2-x1))
+        b = y1 - a*x1
+        self.mouvement = (a,b)
+
+    def move(self):
+        self.position += self.mouvement
+
