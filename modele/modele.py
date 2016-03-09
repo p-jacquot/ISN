cad8ec5e5831925f97eb10d339587290e4a1c81a
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-09 16:35:12 +0100

    Début de l'implémentation des molécules et des touches clavier.

--- modele/modele.py
+++ modele/modele.py
@@ -2,6 +2,8 @@
 import sys
 sys.path.append("../vue")
 from fenetre import Fenetre
+from pygame.event import *
+from pygame.locals import * #Pour les events.
 
 class Modele:
     """La classe qui s'occupera de gérer le jeu en lui même"""
@@ -9,14 +11,54 @@ class Modele:
     def __init__(self, fenetre, niveau): #on lui donnera le niveau qu'on veut jouer (un int, et on ira chercher dans des dossiers).
         self.fenetre = fenetre
         self.changeNiveau(niveau)
-        self.ennemyList = []
+
+        self.ennemyList = [] #les molécules méchantes
+        self.ennemyProjectiles = [] #les projectiles des molécules méchantes
+        self.projectilesJoueur = [] #les projectiles de la molécule gentille.
+
         self.continuer = false
         self.moleculeJoueur = Atome()  #bon ok, c'est un atome...
 
     def play(self):
         while self.continuer:
             #La boucle principale du jeu.
-            print("yolo ! On s'amuse bien !")
+            #print("yolo ! On s'amuse bien !")
+            er = [] #surfaces ennemies
+            epr = [] #surfaces des projectiles ennemis (et pas de l'éducation physique et sportive !)
+            pjr = [] #surfaces des projectiles du joueur.
+            for ennemy in self.ennemyList:
+                ennemy.move()
+                er.append(ennemy.rect)
+            for proj in self.ennemyProjectiles:
+                proj.move()
+                epr.append(proj.rect)
+            for proj in self.projectilesJoueur:
+                proj.move()
+                pjr.append(proj.rect)
+
+            """Events incoming !"""
+            event = pygame.event.poll()
+            if event.type == KEYDOWN:
+                """Lorsqu'on appuie sur une touche. Ces valeurs ne sont là qu'a titre d'exemple, il faudra qu'on les modifies."""
+                if event == K_UP:
+                    self.moleculeJoueur.mv_y = -1
+                elif event == K_DOWN:
+                    self.moleculeJoueur.mv_y = 1
+                elif event == K_LEFT:
+                    self.moleculeJoueur.mv_x = -1
+                elif event == K_RIGHT:
+                    self.moleculeJoueur.mv_x = 1
+            elif event.type == KEYUP:
+                """Lorsqu'on relâche une touche."""
+                if event == K_UP:
+                    self.moleculeJoueur.mv_y = 0
+                elif event == K_DOWN:
+                    self.moleculeJoueur.mv_y = 0
+                elif event == K_LEFT:
+                    self.moleculeJoueur.mv_x = 0
+                elif event == K_RIGHT:
+                    self.moleculeJoueur.mv_x = 0
+            #TODO: Gérer les collisions.
 
     def stop(self):
         #si on veut faire des choses particulières une fois qu'on arrête le jeu.
