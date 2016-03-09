5436810c6f4a9b162f08ceba4e6076f0ce0d37cb
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-09 16:00:30 +0100

    Ecriture du constructeur, et de la fonction sendToVue, du controleur. (miam le bon franglais des familles. :p)

--- controleur/controleur.py
+++ controleur/controleur.py
@@ -3,6 +3,23 @@
 import sys
 sys.path.append("../modele")
 sys.path.append("../vue")
+
 from modele import Modele
 from fenetre import Fenetre
 
+class Controleur:
+    """La classe du controleur, celle qui fait le lien entre le modele, et l'affichage"""
+
+    def __init__(self, modele, fenetre):
+        self.modele = modele
+        self.fenetre = fenetre
+
+    def sendToVue(self, molList, molJoueur):
+        """Envoie les bonnes images en fonction de l'état des molécules."""
+        for mol in molList:
+            if mol.isAlive:
+                self.fenetre.molecules.append([mol.img, mol.pos])
+
+            else:
+                self.fenetre.molecules.append([mol.imgBoum, mol.pos])
+        fenetre.rafraichir()
