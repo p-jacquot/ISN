505a4ccca3446a6564e4ec719943cf2e26710e00
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-11 19:03:31 +0100

    Suppression du dossier modele, qui nous embetait plus qu'autre chose.

--- modele/Sujet.py
+++
@@ -1,9 +0,0 @@
-﻿# Créé par Baptiste Noblet, le 02/03/2016 en Python 3.2
-
-class Sujet:
-    def __init__(self,x=120,y=10):
-        self.x=x  #à changer quand on aura la fenetre
-        self.y=y
-
-
-
