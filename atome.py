2085ddf8d9943d3866f1c70a0193355ba05a8e84
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-12 21:41:44 +0100

    Codage des autres atomes, et ajout d'une fonction seDessiner().

--- atome.py
+++ atome.py
@@ -2,13 +2,15 @@
 
 from math import *
 from projectiles import Projectile
+from pygame.color import *
+from pygame.draw import circle
 
 class Atome:
 
-    def __init__(self, hp, pos, modele) :
+    def __init__(self, hp, posX, posY) :
         self.hp=hp
-        self.position = pos #pos : tuple de la forme (positionX, positionY)
-        self.modele = modele
+        self.posX = posX
+        self.posY = posY
 
     def Boom(self) :
         print("Boom la molécule explose !")
@@ -20,9 +22,9 @@ class Atome:
 
 class Hydrogene(Atome):
 
-    def __init__(self , hp, pos, modele):
-        Atome.__init__(hp, pos, modele)
-        self.atomeVoisin = none
+    def __init__(self , hp, posX, posY):
+        Atome.__init__(hp, posX, posY)
+        self.atomeVoisin = None
         #Attention les enfants, ce constructeur ne sert à rien pour l'instant !
 
 
@@ -31,7 +33,43 @@ class Hydrogene(Atome):
         #print(distanceCible)
         self.modele.add(Projectile(self.position, posCible)) # ce ne sera peut être pas tout à fait la méthode add(), du modèle,
 
-    def seLierA(self, atome):
+    def seDessiner(self, surface):
+        Pygame.draw.circle(surface, WHITE, (posX, posY), 3)
+
+    """def seLierA(self, atome):
         #Ici c'est l'hydrogène, alors il n'a qu'un seul voisin, mais les autres auront une liste de voisins.
         self.atomeVoisin = atome
-        atome.seLierA(self) #Pas sûr de la syntaxe ici.
+        atome.seLierA(self) #Pas sûr de la syntaxe ici."""
+
+class Carbone(Atome):
+
+    def __init__(self, hp, posX, posY):
+        Atome.__init__(hp, posX, posY)
+        self.atomeVoisinHaut = None
+        self.atomeVoisinBas = None
+        self.atomeVoisinGauche = None
+        self.atomeVoisinDroite = None
+
+    def seDessiner(self, surface):
+        Pygame.draw.circle(surface, BLACK, (posX, posY), 10)
+
+class Azote(Atome):
+
+    def __init__(self, hp, posX, posY):
+        Atome.__init__(hp, posX, posY)
+        self.atomeVoisinHaut = None
+        self.atomeVoisinDroit = None
+        self.atomeVoisinGauche = None
+
+    def seDessiner(self, surface):
+        Pygame.draw.circle(surface, BLUE, (posX, posY), 7)
+
+class Oxygene(Atome):
+
+    def __init__(self, hp, posX, posY):
+        Atome.__init__(hp, posX, posY)
+        self.atomeVoisinDroit = None
+        self.atomeVoisinGauche = None
+
+    def seDessiner(self, surface):
+        Pygame.draw.circle(surface, RED, (posX, posY), 5)
