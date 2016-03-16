36f52b2d05e3b2d17b57e97f53d5a79c3f3267ee
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-16 14:11:13 +0100

    Suppression de la fonction lierA(), et d'une ligne étrange dans l'__init__.

--- atome.py
+++ atome.py
@@ -25,19 +25,12 @@ class Hydrogene(Atome):
 
     def __init__(self , hp, posX, posY):
         Atome.__init__(self, hp, posX, posY)
-        self.atomeVoisin = True
-        #Attention les enfants, ce constructeur ne sert à rien pour l'instant !
 
     def tir(self, posCible):
         #distanceCible = int(sqrt(pow(xCible-self.x,2)+pow(yCible-self.y,2)))
         #print(distanceCible)
         self.modele.add(Projectile((self.posX, self.posY), posCible,0 )) # ce ne sera peut être pas tout à fait la méthode add(), du modèle,
         #le troisieme argument est pour différencier d'où vient le projectile (0=ennemi,1=joueur) et aussi pour différencier les deux constructeurs de l'autre coté
-
-    def seLierA(self, atome):
-        #Ici c'est l'hydrogène, alors il n'a qu'un seul voisin, mais les autres auront une liste de voisins.
-        self.atomeVoisin = atome
-        atome.seLierA(self) #Pas sûr de la syntaxe ici.
 
 class Carbone(Atome):
 
