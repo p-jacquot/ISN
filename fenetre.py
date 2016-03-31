75183aa6b9989678f9d88a016596bb284367cad5
Pierre <pierre.jacquot@cassin-macon.fr>
2016-03-31 18:39:41 +0200

    Ajout d'une fonction pour écrire le texte.

--- fenetre.py
+++ fenetre.py
@@ -11,6 +11,7 @@ class Fenetre:
         self.fen = pygame.display.set_mode((largeur, hauteur))
         self.largeur = largeur
         self.hauteur = hauteur
+        self.font = pygame.font.Font(None, 30)
         #self.imgList = []
         self.entites = [] #une liste qui contient des listes comme ça : [image, tuple_de_position]
         #self.fond = fond
@@ -41,6 +42,12 @@ class Fenetre:
     def dessinerCadre(self, posX, posY, hauteur, largeur):
         pygame.draw.rect(self.fen, pygame.Color(255, 255, 255, 0), pygame.Rect(posX, posY, largeur, hauteur))
 
+    def ecrireTexte(self, texte, posX, posY):
+        """Attention, le texte doit être affiché en dernier, car il faut le flip() 'manuellement'."""
+        surface = self.font.render(texte, 0, pygame.Color(255, 0, 0, 0))
+        self.fen.blit(surface, (posX, posY))
+        pygame.display.flip()
+
     def fermer(self):
         pygame.quit()
 
@@ -48,7 +55,8 @@ if __name__ == "__main__":
     f = Fenetre("test", 768, 600)
     f.fond = pygame.image.load("resources/hakase_nyan.png").convert_alpha()
     #f.addImgList(pygame.image.load("hakase_nyan.png").convert_alpha(), (0, 0))
-    f.dessinerCadre(0, 50, 100, 300)
+    #f.dessinerCadre(0, 50, 100, 300)
     f.rafraichir()
+    #f.ecrireTexte("lel", 500, 200)
     time.sleep(2)
     f.fermer()
