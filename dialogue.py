7ce9c7857f94b10482a96a05cc4fa7d571c6969f
Pierre <pierre.jacquot@cassin-macon.fr>
2016-04-04 19:30:31 +0200

    Les dialogues sont serializables.

--- dialogue.py
+++ dialogue.py
@@ -4,8 +4,8 @@ import pygame.image
 class Dialog:
     """Classe qui gère les dialogues lors des niveaux."""
 
-    def __init__(self, img1, nom1, pos1, img2, nom2, pos2):
-        self.characters = [[nom1, img1, pos1], [nom2, img2, pos2]] #La liste des personnages, au cas où il y aurait plus de 2 perso's dans la conversation.
+    def __init__(self, pathImg1, nom1, pos1, pathImg2, nom2, pos2):
+        self.characters = [[nom1, pathImg1, pos1], [nom2, pathImg2, pos2]] #La liste des personnages, au cas où il y aurait plus de 2 perso's dans la conversation.
         #pos1 et pos2 sont des tuples de position !
         #print(self.characters)
         #self.leftPosition = (10, 100)   #Utilisés pour placer les personnages qui parlent sur la fenêtre.
@@ -16,10 +16,12 @@ class Dialog:
 
     def getPunchline(self):
         if self.counter < len(self.punchlineList):
-            punchline = [self.punchlineList[self.counter][0],
+            """punchline = [self.punchlineList[self.counter][0],
                         self.characters[self.punchlineList[self.counter][1]][0],
                         self.characters[self.punchlineList[self.counter][1]][1],
-                        self.characters[self.punchlineList[self.counter][1]][2]]
+                        self.characters[self.punchlineList[self.counter][1]][2]]"""
+            punchline = [self.punchlineList[self.counter][0],
+                            self.punchlineList[self.counter][1]]
             self.counter += 1
             if self.counter >= len(self.punchlineList):
                 self.notFinished = False
