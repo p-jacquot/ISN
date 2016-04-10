47774e81abd5f199784912c4d8f423a232e9f91f
DantCold <dantcollide@gmail.com>
2016-04-10 11:06:04 +0200

    ajout pattern sinusoidal

--- pattern.py
+++ pattern.py
@@ -14,20 +14,20 @@ class Pattern:
         posY += self.mv_y
         return posX, posY
 
-class PatternDroit(Pattern):
+class PatternPolynome(Pattern):
     """Pattern qui fait se déplacer selon un polynome du second degré."""
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
-        self.c = c
-        if c == sqrt(pow(x,2)):
+        self.c = abs(c)  #je pense pas qu'on aura une ordonnée à l'origine en dehors de l'écran
+        if c == sqrt(pow(x,2)):# le signe de c donne la direction de x
             self.dir = 1
         else:
             self.dir = -1
 
     def deplacer(self, posX, posY):
         posX += self.dir
-        posY = self.a * posX * posX + self.b * posX + self.c
+        posY = self.a * posX * posX + self.b * posX - self.c
         return posX, posY
 
 class PatternCercle(Pattern):
@@ -46,10 +46,10 @@ class PatternCercle(Pattern):
         return posX,posY
 
 class PatternZigZag(Pattern):#je ne suis pas sûr de la syntaxe pour ce pattern
-    def __init__(self,temps):
-        self.tempsMax=abs(temps,vitesse)#temps entre les changements de direction
+    def __init__(self,temps,vitesseMax):
+        self.tempsMax=abs(temps,vitesse)#temps entre les changements de direction(en nb defois où on appelle move)
         self.compteur=-2
-        self.vitesse=abs(vitesse)
+        self.vitesse=abs(vitesseMax)
 
     def deplacer(self,posX,posY):
         compteur+=1
@@ -59,4 +59,14 @@ class PatternZigZag(Pattern):#je ne suis pas sûr de la syntaxe pour ce pattern
             self.compteur=-2
 
         return self.Pattern.deplacer(posX,posY)
+
+class PatternSinusoidal(Pattern):
+    def __init__(self,amplitude,direction):
+        self.amplitude=amplitude
+        self.direction=direction
+
+    def deplacer(posX,posY):
+        posX+=self.direction
+        posY = self.amplitude *sin(posX/180*pi)
+        return posX,posY
 
