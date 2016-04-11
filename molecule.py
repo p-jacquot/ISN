46171fb547d312c155ed7f64a138bac878eb5247
DantCold <dantcollide@gmail.com>
2016-04-11 23:06:45 +0200

    j'empeche mnt le joueur de sortir

--- molecule.py
+++ molecule.py
@@ -58,8 +58,18 @@ class Molecule:
         self.rect.x = self.posX
         self.rect.y = self.posY
         #TODO: ici, prendre la décision de tirer ou non.
-        if self.posX+largeur<-5 or self.posX>355 or self.posY+hauteur<-5 or self.posY>768+5:#changer valeur ici aussi
+        if self.posX+largeur<-5 or self.posX>355 or self.posY+hauteur<-5 or self.posY>768+5:     #changer valeur ici aussi
             self.__del__()   #pas besoin de passer par hit, il n'y aura pas d'animation comme c'est hors de l'écran
+
+        if self.__name__()=="moleculeJoueur":  #condition qui regarde si le joueur ne dépasse pas les limites de l'entendement
+            if self.posX<10:
+                self.posX=10
+            elif self.posX>340:  #changer encore ici
+                self.posX=340
+            if self.posY <10:
+                self.posY=10
+            elif self.posY>758:
+                self.posY=758
 
     def tirer(self):
         projectiles = []
