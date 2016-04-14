c16c7ba7859f37fdcbe9564fe94a04c7e0668c0a
DantCold <dantcollide@gmail.com>
2016-04-14 13:03:41 +0200

    mise en comme du mauvais constructeur

--- projectiles.py
+++ projectiles.py
@@ -10,18 +10,18 @@ class Projectile:
         self.mouvement = [mv[0],mv[1]] #liste de mouvement.
         self.dead=False
 
-    def __init__(self, pos, posCible,equipe):
-        """Le fameux constructeur qui va permettre de viser le joueur."""
+    """ def __init__(self, pos, posCible,equipe):
+        #Le fameux constructeur qui va permettre de viser le joueur.
         self.position = pos
         x1, y1 = pos
         x2, y2 = posCible
         #a = int((y2-y1)/(x2-x1))
         #b = y1 - a*x1
         distance=sqrt(pow(x2-x1,2)+pow(y2-y1,2))
-        """on calcule le module et presque l'argument du vecteur atome cible"""
+        #on calcule le module et presque l'argument du vecteur atome cible
         a = (x2-x1)/distance
         b = (y2-y1)/distance
-        self.mouvement = (a,b)
+        self.mouvement = (a,b)"""
 
     def move(self):
         self.position[0]+=self.mouvement[0]
