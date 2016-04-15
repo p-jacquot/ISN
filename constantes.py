7c06c27ad2ba579418219e295f7edd897d78bd5e
Pierre <pierre.jacquot@cassin-macon.fr>
2016-04-15 14:38:36 +0200

    Ajout du convert_alpha() aux images, faites moi confiance.

--- constantes.py
+++ constantes.py
@@ -2,13 +2,13 @@
 import pygame
 
 """Constantes projectiles"""
-
-projectilesList = [ pygame.image.load("resources/photos/photon_bleu_clair_pour_le_joueur2.png"),
-                    pygame.image.load("resources/photos/photon_bleu.png"),
-                    pygame.image.load("resources/photos/photon_jaune.png"),
-                    pygame.image.load("resources/photos/photon_rose2.png"),
-                    pygame.image.load("resources/photos/photon_sombre.png"),
-                    pygame.image.load("resources/photos/photon_vert.png"),
-                    pygame.image.load("resources/photos/photon_violet.png"),]
+pygame.init()
+projectilesList = [ pygame.image.load("resources/photos/photon_bleu_clair_pour_le_joueur2.png").convert_alpha(),
+                    pygame.image.load("resources/photos/photon_bleu.png").convert_alpha(),
+                    pygame.image.load("resources/photos/photon_jaune.png").convert_alpha(),
+                    pygame.image.load("resources/photos/photon_rose2.png").convert_alpha(),
+                    pygame.image.load("resources/photos/photon_sombre.png").convert_alpha(),
+                    pygame.image.load("resources/photos/photon_vert.png").convert_alpha(),
+                    pygame.image.load("resources/photos/photon_violet.png").convert_alpha()]
 
 #Quand vous voulez récupérer l'image d'un photon : projectilesList[index]
