c82fa63dafe274b2fff246319ab6ace2e8ca5fca
DantCold <dantcollide@gmail.com>
2016-04-15 13:52:03 +0200

    baka pas de /dossier quand on veut ouvrir un truc

--- constantes.py
+++ constantes.py
@@ -2,12 +2,12 @@
 import pygame
 
 """Constantes projectiles"""
-projectilesList = [ pygame.image.load("/resources/photos/photon_bleu_clair_pour_le_joueur2.png"),
-                    pygame.image.load("/resources/photos/photon_bleu.png"),
-                    pygame.image.load("/resources/photos/photon_jaune.png"),
-                    pygame.image.load("/resources/photos/photon_rose2.png"),
-                    pygame.image.load("/resources/photos/photon_sombre.png"),
-                    pygame.image.load("/resources/photos/photon_vert.png"),
-                    pygame.image.load("/resources/photos/photon_violet.png"),]
+projectilesList = [ pygame.image.load("resources/photos/photon_bleu_clair_pour_le_joueur2.png"),
+                    pygame.image.load("resources/photos/photon_bleu.png"),
+                    pygame.image.load("resources/photos/photon_jaune.png"),
+                    pygame.image.load("resources/photos/photon_rose2.png"),
+                    pygame.image.load("resources/photos/photon_sombre.png"),
+                    pygame.image.load("resources/photos/photon_vert.png"),
+                    pygame.image.load("resources/photos/photon_violet.png"),]
 
 #Quand vous voulez récupérer l'image d'un photon : projectilesList[index]
