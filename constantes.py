7d2e32ffa21a3ee7b6be6cf455dc0569990320f1
DantCold <dantcollide@gmail.com>
2016-04-15 14:16:12 +0200

    je met la photo pour le joueur en premier

--- constantes.py
+++ constantes.py
@@ -2,13 +2,9 @@
 import pygame
 
 """Constantes projectiles"""
-<<<<<<< HEAD
+
 projectilesList = [ pygame.image.load("resources/photos/photon_bleu_clair_pour_le_joueur2.png"),
                     pygame.image.load("resources/photos/photon_bleu.png"),
-=======
-projectilesList = [pygame.image.load("resources/photos/photon_bleu.png"),
-                    pygame.image.load("resources/photos/photon_bleu_clair_pour_le_joueur2.png"),
->>>>>>> 7c48705d2dd2eb653406e84dd764d749d901ccf4
                     pygame.image.load("resources/photos/photon_jaune.png"),
                     pygame.image.load("resources/photos/photon_rose2.png"),
                     pygame.image.load("resources/photos/photon_sombre.png"),
