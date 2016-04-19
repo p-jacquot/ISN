70dbf2f725d084acc726a348ccd72c304661036b
DantCold <dantcollide@gmail.com>
2016-04-19 20:08:46 +0200

    debug
     Please enter the commit message for your changes. Lines starting

--- projectiles.py
+++ projectiles.py
@@ -18,9 +18,11 @@ class Projectile:
         self.rect = self.img.get_rect()
         self.rect.x = self.posX
         self.rect.y = self.posY
+    def __del__(self):
+        del self
 
-    def __init__(self, posX, posY, mv_x, mv_y, indexImg):
-        """Constructeur quasi identique, mais on peut choisir l'image du projectile. (pour ceux du joueur, ce sera tous les mêmes)"""
+    """    def __init__(self, posX, posY, mv_x, mv_y, indexImg):
+        #Constructeur quasi identique, mais on peut choisir l'image du projectile. (pour ceux du joueur, ce sera tous les mêmes)
         self.posX = posX
         self.posY = posY
         self.mv_x = mv_x
@@ -29,7 +31,7 @@ class Projectile:
         self.img = constantes.projectilesList[indexImg].convert_alpha()
         self.rect = self.img.get_rect()
         self.rect.x = self.posX
-        self.rect.y = self.posY
+        self.rect.y = self.posY"""
 
     """ def __init__(self, pos, posCible,equipe):
         #Le fameux constructeur qui va permettre de viser le joueur.
@@ -45,8 +47,10 @@ class Projectile:
         self.mouvement = (a,b)"""
 
     def move(self):
-        self.posX += mv_x
-        self.posY += mv_y
-        self.rect.move_ip(mv_x, mv_y) #On fait bouger le rectangle !
-        if self.posY<-20 or self.posY>610 or self.posX[0]<-20 or self.posX>380:
+        self.posX += self.mv_x
+        self.posY += self.mv_y
+        self.rect.move_ip(self.mv_x, self.mv_y) #On fait bouger le rectangle !
+        if self.posY<-20 or self.posY>610 or self.posX<-20 or self.posX>380:
             self.dead=True
+
+
