36cfe109d694f026a7e9aea852c4c32090242fa6
Pierre <pierre.jacquot@cassin-macon.fr>
2016-04-20 10:30:39 +0200

    La musique se répètera 5 fois.

--- test.py
+++ test.py
@@ -10,11 +10,12 @@ from pattern import *
 import pickle
 import niveau
 pygame.init()
+
 def testplay():
     jeu.moleculeJoueur = Molecule('oxygene.png', 30, 30,Pattern(0,0))
     jeu.moleculeJoueur.posX = 250
     jeu.moleculeJoueur.posY = 500
-    jeu.moleculeJoueur.rect = jeu.moleculeJoueur.rect.move(50, 0)
+    jeu.moleculeJoueur.rect = jeu.moleculeJoueur.rect.move(250, 500)
     jeu.moleculeJoueur.hp =9001
     """jeu.ennemyList.append(Molecule('azote.png', 35, 35,PatternCercle(150,60,25,4,2)))
     jeu.ennemyList.append(Molecule('oxygene.png', 22, 22,PatternZigZag(20,1)))
@@ -24,6 +25,7 @@ def testplay():
         a.posX =randint(15,200)
         a.posY = randint(15,200)"""
     #jeu.ennemyList.append(Molecule('hydrogene.png', 22, 22,PatternSinusoidal(5,1)))
+    jeu.progressInLevel()
     jeu.play()
 
 def testDialog():
@@ -43,6 +45,7 @@ def testSerializedDialogue():
     jeu.dialoguer(dialogue)
 
 pygame.init()
+pygame.mixer.init()
 fenetre = Fenetre("test ISN Dialogue", 768, 600)
 fenetre.fond = pygame.image.load("resources/galaxie.jpg").convert_alpha()
 
